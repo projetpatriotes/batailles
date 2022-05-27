6d5e1cec021631fa3cba65a999eec6794d291dea
kqtn <103220580+kqtn@users.noreply.github.com>
2022-05-27 10:02:35 -0400

    Accueil

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,27 @@
   <div>
     <NavBar />
     <BodyBg titre="Projet Patriotes" :noarrow="true"></BodyBg>
+    <ParagrapheTexte>
+        <NuxtContent class="markdown-content" :document="page" />
+      </ParagrapheTexte>
     <FooterBar />
   </div>
 </template>
+
+<script>
+export default {
+  async asyncData({ $content }) {
+    const page = await $content('index').fetch();
+    return { page };
+  }
+};
+</script>
+
+<style lang="scss">
+.markdown-content {
+  color: $text;
+  background-color: $second-background;
+  border-radius: 1vw;
+  padding: 2.5rem;
+}
+</style>
